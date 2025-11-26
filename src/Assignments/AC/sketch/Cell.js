fb17eb9971fdc9f9fe67c341426d0f5e6d2b699f
kwonsubak <233090449+kwonsubak@users.noreply.github.com>
2025-11-27 01:15:01 +0900

    1127

---
+++ src/Assignments/AC/sketch/Cell.js
@@ -0,0 +1,93 @@
+const state = {
+  R: 'R',
+  P: 'P',
+  S: 'S',
+};
+
+class Cell {
+  pos = [0, 0];
+  size = [0, 0];
+  state = random(this.state);
+  neighbors = [null, null, null, null, null, null, null, null];
+
+  constructor(x, y, w, h, state = this.state) {
+    this.pos = [x, y];
+    this.size = [w, h];
+    this.state = state;
+  }
+
+  setNeighbors(tl, t, tr, r, br, b, bl, l) {
+    this.neighbors[0] = tl;
+    this.neighbors[1] = t;
+    this.neighbors[2] = tr;
+    this.neighbors[3] = r;
+    this.neighbors[4] = br;
+    this.neighbors[5] = b;
+    this.neighbors[6] = bl;
+    this.neighbors[7] = l;
+  }
+
+  computeNextState() {
+    //filter: array 내장함수, filter: return 필수. return 뒤에 T인 것만 필터링함.
+    const neighborStates = this.neighbors.filter(n => n !== null).map(n => n.state);
+
+    const neighborsDead = this.neighbors.filter(n => n !== null);
+
+    if (neighborStates.includes('R') && this.state === 'S') {
+      this.nextState = 'R';
+      return;
+    } else if (neighborStates.includes('S') && this.state === 'P') {
+      this.nextState = 'S';
+      return;
+    } else if (neighborStates.includes('P') && this.state === 'R') {
+      this.nextState = 'P';
+      return;
+    }
+
+    if (neighborStates.includes('R') && this.state === 'S') {
+      this.nextState = 'R';
+    } else if (neighborStates.includes('S') && this.state === 'P') {
+      this.nextState = 'S';
+    } else if (neighborStates.includes('P') && this.state === 'R') {
+      this.nextState = 'P';
+    } else {
+      this.nextState = this.state;
+    }
+
+    const win = {
+      R: 'S',
+      S: 'P',
+      P: 'R',
+    };
+
+    const iWinAgainst = win[this.state];
+
+    const canEat = neighborStates.includes(iWinAgainst);
+
+    if (canEat) {
+      this.nextState = this.state;
+    } else {
+      this.nextState = this.state;
+    }
+
+    if (canEat) {
+      neighborsDead.forEach(n => {
+        if (n.state === iWinAgainst) {
+          n.nextState = state;
+        }
+      });
+    }
+  }
+
+  updateState() {
+    this.state = this.nextState;
+  }
+
+  render() {
+    if (this.state === 'R') fill('#FF8F8F');
+    else if (this.state === 'P') fill('#FFF1CB');
+    else if (this.state === 'S') fill('#B7A3E3');
+
+    rect(this.pos[0], this.pos[1], this.size[0], this.size[1]);
+  }
+}
---
+++ src/Assignments/AC/sketch/main.js
@@ -0,0 +1,117 @@
+const canvasContainer = document.getElementById('canvas-container');
+let renderer;
+
+const INITIAL_W = 800;
+const INITIAL_H = 600;
+const INITIAL_RATIO = INITIAL_W / INITIAL_H;
+
+let lastTime = 0;
+const interval = 300;
+
+const cellsPerRow = 60;
+//한 줄당 몇개의 셀이 들어가는가?
+let cellsPerColumn;
+const cells = [];
+let cellSize;
+
+function getIdx(r, c) {
+  return r * cellsPerRow + c;
+}
+
+function setup() {
+  renderer = createCanvas(INITIAL_W, INITIAL_H);
+  renderer.parent(canvasContainer);
+  renderer.elt.style.aspectRatio = `${INITIAL_W} / ${INITIAL_H}`;
+
+  new ResizeObserver(() => {
+    const { width: containerWidth, height: containerHeight } = canvasContainer.getBoundingClientRect();
+    renderer.elt.style.width = `${containerWidth}px`;
+    renderer.elt.style.height = `${containerWidth / INITIAL_RATIO}px`;
+  }).observe(canvasContainer);
+
+  cellSize = width / cellsPerRow;
+  cellsPerColumn = Math.floor(height / cellSize);
+
+  const states = ['R', 'P', 'S'];
+
+  //안에꺼 먼저 만들어 줘야함.
+  for (let r = 0; r < cellsPerColumn; r++) {
+    for (let c = 0; c < cellsPerRow; c++) {
+      const x = c * cellSize;
+      const y = r * cellSize;
+      const randomState = random(states);
+      const newCell = new Cell(x, y, cellSize, cellSize, randomState);
+      cells.push(newCell);
+    }
+  }
+
+  cells.forEach((cell, idx) => {
+    const row = Math.floor(idx / cellsPerRow);
+    const col = idx % cellsPerRow;
+    const tl = row > 0 && col > 0 ? cells[getIdx(row - 1, col - 1)] : null;
+    const t = row > 0 ? cells[getIdx(row - 1, col)] : null;
+    const tr = row > 0 && col < cellsPerRow - 1 ? cells[getIdx(row - 1, col + 1)] : null;
+    const r = col < cellsPerRow - 1 ? cells[getIdx(row, col + 1)] : null;
+    const br = row < cellsPerColumn - 1 && col < cellsPerRow - 1 ? cells[getIdx(row + 1, col + 1)] : null;
+    const b = row < cellsPerColumn - 1 ? cells[getIdx(row + 1, col)] : null;
+    const bl = row < cellsPerColumn - 1 && col > 0 ? cells[getIdx(row + 1, col - 1)] : null;
+    const l = col > 0 ? cells[getIdx(row, col - 1)] : null;
+    cell.setNeighbors(tl, t, tr, r, br, b, bl, l);
+  });
+}
+
+function showStats() {
+  let rCount = 0;
+  let pCount = 0;
+  let sCount = 0;
+
+  cells.forEach(cell => {
+    if (cell.state === 'R') rCount++;
+    else if (cell.state === 'P') pCount++;
+    else if (cell.state === 'S') sCount++;
+  });
+
+  const total = cells.length;
+
+  const rRatio = ((rCount / total) * 100).toFixed(3);
+  const pRatio = ((pCount / total) * 100).toFixed(3);
+  const sRatio = ((sCount / total) * 100).toFixed(3);
+
+  const table = INITIAL_H - 15;
+
+  push();
+  noStroke();
+  fill('#B7A3E3');
+  rect(0, INITIAL_H - 40, (INITIAL_W / 3) * 3, 50);
+  fill('#FFF1CB');
+  rect(0, INITIAL_H - 40, (INITIAL_W / 3) * 2, 50);
+  fill('#FF8F8F');
+  rect(0, INITIAL_H - 40, INITIAL_W / 3, 50);
+  pop();
+
+  push();
+  fill(0);
+  textAlign(CENTER);
+  textSize(18);
+  text(`R: ${rRatio}%`, (INITIAL_W / 3) * 0.5, table);
+  text(`P: ${pRatio}%`, (INITIAL_W / 3) * 1.5, table);
+  text(`S: ${sRatio}%`, (INITIAL_W / 3) * 2.5, table);
+  pop();
+}
+
+function draw() {
+  background(250);
+
+  cells.forEach(cell => cell.computeNextState());
+
+  if (millis() - lastTime > interval) {
+    cells.forEach(aCell => {
+      aCell.updateState();
+    });
+    lastTime = millis();
+  }
+
+  cells.forEach(cell => cell.render());
+
+  showStats();
+}
